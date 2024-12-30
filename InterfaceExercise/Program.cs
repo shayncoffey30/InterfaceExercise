9c57c694dcdcd592e017b40e4f64a2648aabd3da
Shayn Coffey <Shayncoffey30@gmail.com>
2024-12-30 12:52:51 -0500

    excercise completed

--- InterfaceExercise/Program.cs
+++ InterfaceExercise/Program.cs
@@ -1,49 +1,29 @@
-﻿using System;
+﻿using InterfaceExercise;
+using System;
+using System.Collections.Generic;
 
 namespace InterfaceExercise
-{
-    class Program
+{ 
+    public class Program
     {
+
         static void Main(string[] args)
         {
-            //TODO Be sure to follow BEST PRACTICES when creating classes and interfaces
 
-            //Create 2 Interfaces called IVehicle & ICompany
+            var car = new Car();
 
-            //Create 3 classes called Car , Truck , & SUV
+            var truck = new Truck();
+            var vehicles = new List<IVehicle>() { car, truck };
 
-            //In your IVehicle:
-            
-                /* Create 4 members that Car, Truck, & SUV all have in common.
-                 * Example: public int NumberOfWheels { get; set; }
-                 */
-            
+            foreach (var vehicle in vehicles)
+            {
+                vehicle.Drive();
+                vehicle.ChangeGears(true);
+                vehicle.Reverse();
 
-            //In ICompany: 
-            
-                /* Create 2 members that are specific to each every company
-                 * regardless of vehicle type.
-                 * Example: public string Logo { get; set; }
-                 */
 
-            //In each of your Car, Truck, and SUV classes
-
-                /* Create 2 members that are specific to each class
-                 * Example for Car: public bool HasTrunk { get; set; }
-                 * Example for SUV: public int NumberOfSeats { get; set; }
-                 *
-                 * Then, Set each class to inherit from both IVehicle and ICompany and implement their members.
-                 */
-
-            //Now, create objects of your 3 classes and give their members values.
-            //Creatively display and organize their values
-            
-            //Option for displaying values: 
-            //Create a stubbed out method called DisplayDetails in your IVehicle interface.
-            //Implement the stubbed out method in the derived classes.
-            //In the scope of them method, use string interpolation to display property values.
-            //In order to also interpolate values from ICompany, research how to extend interfaces.
-            
+            }
         }
-    }
+    }    
+ 
 }
